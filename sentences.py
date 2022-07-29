b2b70f49b0c916278b28afa831ddba8948249c91
Makale Lord <110202582+MakaleLord@users.noreply.github.com>
2022-07-28 19:36:44 -0500

    Add files via upload

---
+++ sentences.py
@@ -0,0 +1,62 @@
+
+<style>
+@import url('https://fonts.googleapis.com/css?family=Roboto+Mono');
+html{
+   display:flex;
+   justify-content:center;
+   align-items: center;
+   width: 100%;
+   height: 100%;
+   background-image: url('https://source.unsplash.com/collection/327760/1600x900');
+   background-size:cover;
+}
+
+body{
+   width:55%;
+   height:60vh;
+   border:1px solid lightgray;
+   background-color:white;
+   border-radius:8px;
+   font-family: 'Roboto Mono', monospace;
+   box-shadow: 0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22);
+   border-top:32px solid #eee;
+   overflow-y:scroll;
+}
+
+body > div{
+  height: calc(100% - 40px);
+  overflow-y:auto;
+  padding: 20px;
+  font-size: 24px;
+  white-space: pre;
+}
+
+pre{
+  margin:inherit;
+  font-family: inherit;
+}
+</style>
+
+<div>
+Can you complete the sentances:
+
+___ toy _______ back fond memories 
+
+You have _____ _____ __ be angry 
+
+_ liked their first ___ albums 
+
+He _____ ______ bananas in ___ dog buns 
+
+
+
+
+
+
+
+
+Correct Answers:
+The toy brought back fond memories
+You have every right to be angry
+I liked their first two albums
+He loved eating bananas in hot dog buns
